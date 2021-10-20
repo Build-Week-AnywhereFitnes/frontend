490793db1369c0368bcfe880ec59ac5a71b3ae91
LuiCoro <luiacorona029@icloud.com>
2021-10-20 14:43:42 -0700

    InstructorPage.js 'is getting all classes data correctly' 'renders everything' -edit and delete just need functionality

--- fitness-front-end/src/components/InstructorPage.js
+++ fitness-front-end/src/components/InstructorPage.js
@@ -1,46 +1,76 @@
-import React, {useState} from 'react'
-import {Table, Button} from "semantic-ui-react";
+import React, {useState, useEffect} from 'react'
+import {Table, Button, } from "semantic-ui-react";
+import axios from 'axios'
+
+
 
 const InstructorPage = () => {
+
+  const [Classes, setClasses] = useState([])
+
+  // get all the classes -> /api/classes/
+
+  //get the class info with id -> /api/classes/:id
+
+  //update class info with the id -> /api/classes/:id
+
+  console.log(Classes)
+
+  const fetchClasses = async () => {
+    const data = await fetch('http://localhost:5500/api/classes')
+
+    const classList = await data.json()
+    console.log(classList.classes)
+    setClasses(classList.classes)
+    console.log('Data -> ', Classes)
+  }
+
+  useEffect(() => {
+    fetchClasses()
+  }, [])
 
 
   return (
       <div>
-        <Table columns={8}>
+        <Table columns={9}>
           <Table.Header>
             <Table.Row>
+              <Table.HeaderCell>Class ID</Table.HeaderCell>
               <Table.HeaderCell>Class Name</Table.HeaderCell>
               <Table.HeaderCell>Class Type</Table.HeaderCell>
               <Table.HeaderCell>Start Time</Table.HeaderCell>
               <Table.HeaderCell>Duration</Table.HeaderCell>
               <Table.HeaderCell>Intensity Level</Table.HeaderCell>
               <Table.HeaderCell>Location</Table.HeaderCell>
-              <Table.HeaderCell>Max Number of Attendees</Table.HeaderCell>
               <Table.HeaderCell>Max Class Size</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
-            <Table.Row>
-              <Table.Cell>Yoga</Table.Cell>
-              <Table.Cell>Mediation</Table.Cell>
-              <Table.Cell>7:00AM</Table.Cell>
-              <Table.Cell>2 Hours</Table.Cell>
-              <Table.Cell>Hard</Table.Cell>
-              <Table.Cell>Mars, OuterSpace</Table.Cell>
-              <Table.Cell>5</Table.Cell>
-              <Table.Cell>20</Table.Cell>
-            </Table.Row>
+            {Classes.map(classInfo => (
+                <Table.Row key={classInfo.class_id}>
+                  <Table.Cell>{classInfo.class_id}</Table.Cell>
+                  <Table.Cell>{classInfo.className}</Table.Cell>
+                  <Table.Cell>{classInfo.classType}</Table.Cell>
+                  <Table.Cell>{classInfo.startTime}</Table.Cell>
+                  <Table.Cell>{classInfo.duration}</Table.Cell>
+                  <Table.Cell>{classInfo.intensityLevel}</Table.Cell>
+                  <Table.Cell>{classInfo.location}</Table.Cell>
+                  <Table.Cell>{classInfo.classMAX}</Table.Cell>
+                </Table.Row>
+            ))}
+
+
           </Table.Body>
 
           <Table.Footer>
             <Table.Row>
-              <Table.HeaderCell />
-              <Table.HeaderCell />
-              <Table.HeaderCell />
-              <Table.HeaderCell />
-              <Table.HeaderCell />
-              <Table.HeaderCell />
+              <Table.HeaderCell/>
+              <Table.HeaderCell/>
+              <Table.HeaderCell/>
+              <Table.HeaderCell/>
+              <Table.HeaderCell/>
+              <Table.HeaderCell/>
               <Table.HeaderCell>
                 <Button>Edit</Button>
               </Table.HeaderCell>
